fa1d6d630774799544aad8e3ebbdec25ba38f1ca
Raz's Laptop <razgi.developer@gmail.com>
2020-09-15 14:07:35 +0300

    finished polishing all of the screens

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -21,8 +21,13 @@ class _MainMenuState extends State<MainMenu> {
       initialScrollOffset: 0.0,
       keepScrollOffset: true,
     );
+  }
 
+  @override
+  void didChangeDependencies() {
     Future.delayed(Duration(milliseconds: 300)).then((value) => _toEnd());
+
+    super.didChangeDependencies();
   }
 
   void _toEnd() {
@@ -40,100 +45,103 @@ class _MainMenuState extends State<MainMenu> {
       appBar: MyAppBar(context),
       extendBodyBehindAppBar: true,
       body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                "assets/images/background.jpg",
-              ),
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/background.jpg",
               fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                  margin: EdgeInsets.only(top: screenSize.height / 25),
-                  height: screenSize.height / 5,
-                  width: screenSize.height / 5,
-                  child: Image.asset('assets/images/logo.png')),
-              SizedBox(height: screenSize.height / 50),
-              TypewriterAnimatedTextKit(
-                  speed: Duration(milliseconds: 100),
-                  totalRepeatCount: 1,
-                  repeatForever: false,
-                  pause: Duration(milliseconds: 1000),
-                  text: ["Welcome to noline, connecting lines online."],
-                  textStyle: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      .merge(TextStyle(color: Colors.black87)),
-                  displayFullTextOnTap: true,
-                  stopPauseOnTap: true),
-              //Container(child: Image.asset('assets/images/illustration.png')),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                controller: _scrollController,
-                child: Container(
-                  width: screenSize.height * 2,
-                  height: screenSize.height / 3,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        'assets/images/illustration.png',
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      margin: EdgeInsets.only(top: screenSize.height / 25),
+                      height: screenSize.height / 5,
+                      width: screenSize.height / 5,
+                      child: Image.asset('assets/images/logo.png')),
+                  SizedBox(height: screenSize.height / 50),
+                  TypewriterAnimatedTextKit(
+                      speed: Duration(milliseconds: 100),
+                      totalRepeatCount: 1,
+                      repeatForever: false,
+                      pause: Duration(milliseconds: 1000),
+                      text: ["Welcome to noline, connecting lines online."],
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .merge(TextStyle(color: Colors.black87)),
+                      displayFullTextOnTap: true,
+                      stopPauseOnTap: true),
+                  //Container(child: Image.asset('assets/images/illustration.png')),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    controller: _scrollController,
+                    child: Container(
+                      width: screenSize.height * 2,
+                      height: screenSize.height / 3,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/illustration.png',
+                          ),
+                          fit: BoxFit.fill,
+                          repeat: ImageRepeat.noRepeat,
+                        ),
                       ),
-                      fit: BoxFit.fill,
-                      repeat: ImageRepeat.noRepeat,
                     ),
                   ),
-                ),
+                  SizedBox(height: screenSize.height / 20),
+                  Container(
+                    width: screenSize.height * 0.7,
+                    child: RaisedButton(
+                        color: Colors.white.withOpacity(0.20),
+                        onPressed: () {
+                          // Move to "enter line code page"
+                          Navigator.pushNamed(context, '/join-line');
+                        },
+                        child: Container(
+                          width: screenSize.width / 4,
+                          height: screenSize.height / 15,
+                          child: FittedBox(
+                              child: Text(
+                            'Join a line',
+                            style: Theme.of(context).textTheme.headline1.merge(
+                                TextStyle(
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.w300)),
+                          )),
+                        )),
+                  ),
+                  SizedBox(height: buttonsDistance),
+                  Container(
+                    width: screenSize.height * 0.7,
+                    child: RaisedButton(
+                        color: Colors.white.withOpacity(0.20),
+                        onPressed: () {
+                          // Move to "enter line code page"
+                          Navigator.pushNamed(context, '/manager-login');
+                        },
+                        child: Container(
+                          width: screenSize.width / 2,
+                          height: screenSize.height / 15,
+                          child: FittedBox(
+                              child: Text(
+                            'Manage a line of your own',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1
+                                .merge(TextStyle(color: Colors.black87)),
+                          )),
+                        )),
+                  ),
+                ],
               ),
-              SizedBox(height: screenSize.height / 20),
-              Container(
-                width: screenSize.height * 0.7,
-                child: RaisedButton(
-                    color: Colors.white.withOpacity(0.20),
-                    onPressed: () {
-                      // Move to "enter line code page"
-                      Navigator.pushNamed(context, '/join-line');
-                    },
-                    child: Container(
-                      width: screenSize.width / 4,
-                      height: screenSize.height / 15,
-                      child: FittedBox(
-                          child: Text(
-                        'Join a line',
-                        style: Theme.of(context).textTheme.headline1.merge(
-                            TextStyle(
-                                color: Colors.black87,
-                                fontWeight: FontWeight.w300)),
-                      )),
-                    )),
-              ),
-              SizedBox(height: buttonsDistance),
-              Container(
-                width: screenSize.height * 0.7,
-                child: RaisedButton(
-                    color: Colors.white.withOpacity(0.20),
-                    onPressed: () {
-                      // Move to "enter line code page"
-                      Navigator.pushNamed(context, '/manager-login');
-                    },
-                    child: Container(
-                      width: screenSize.width / 2,
-                      height: screenSize.height / 15,
-                      child: FittedBox(
-                          child: Text(
-                        'Manage a line of your own',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline1
-                            .merge(TextStyle(color: Colors.black87)),
-                      )),
-                    )),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
