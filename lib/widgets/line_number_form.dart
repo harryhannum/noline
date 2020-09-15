ecabd349be02b767a8cf5671381e446e415596be
Raz's Laptop <razgi.developer@gmail.com>
2020-09-15 19:42:42 +0300

    fixed the fucking submit button

--- lib/widgets/line_number_form.dart
+++ lib/widgets/line_number_form.dart
@@ -13,7 +13,7 @@ class LineNumberForm extends StatefulWidget {
 }
 
 class _LineNumberFormState extends State<LineNumberForm> {
-  String test = "";
+  String textbb = "";
 
   @override
   Widget build(BuildContext context) {
@@ -36,73 +36,80 @@ class _LineNumberFormState extends State<LineNumberForm> {
       return double.parse(s, (e) => null) != null;
     }
 
-    return Form(
-      child: Column(
-        children: [
-          Container(
-            width: screenSize.height * 0.5,
-            child: InkWell(
-              onTap: () => _pinPutFocusNode.requestFocus(),
-              child: PinPut(
-                fieldsCount: 4,
-                eachFieldHeight: screenSize.height * 0.07,
-                eachFieldWidth: screenSize.height * 0.06,
-                textStyle: Theme.of(context)
-                    .textTheme
-                    .headline4
-                    .merge(TextStyle(color: Colors.black87)),
-                onSubmit: (String pin) {},
-                focusNode: _pinPutFocusNode,
-                key: LineNumberForm.formKey,
-                controller: _pinPutController,
-                submittedFieldDecoration: pinPutDecoration.copyWith(
-                    borderRadius: BorderRadius.circular(12.0),
-                    border: Border.all(color: Colors.black12)),
-                selectedFieldDecoration: pinPutDecoration,
-                followingFieldDecoration: pinPutDecoration.copyWith(
-                  border: Border.all(
-                    color: Colors.black54,
+    return Column(
+      children: [
+        Form(
+          child: Column(
+            children: [
+              Container(
+                width: screenSize.height * 0.5,
+                child: InkWell(
+                  onTap: () => _pinPutFocusNode.requestFocus(),
+                  child: PinPut(
+                    fieldsCount: 4,
+                    eachFieldHeight: screenSize.height * 0.07,
+                    eachFieldWidth: screenSize.height * 0.06,
+                    textStyle: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .merge(TextStyle(color: Colors.black87)),
+                    onSubmit: (String pin) {
+                      setState(() {
+                        textbb = pin;
+                      });
+                    },
+                    key: LineNumberForm.formKey,
+                    focusNode: _pinPutFocusNode,
+                    controller: _pinPutController,
+                    submittedFieldDecoration: pinPutDecoration.copyWith(
+                        borderRadius: BorderRadius.circular(12.0),
+                        border: Border.all(color: Colors.black12)),
+                    selectedFieldDecoration: pinPutDecoration,
+                    followingFieldDecoration: pinPutDecoration.copyWith(
+                      border: Border.all(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    validator: (string) {
+                      return (isNumeric(string) && int.parse(string) > 999)
+                          ? null
+                          : "Please enter a valid Code";
+                    },
                   ),
                 ),
-                validator: (string) {
-                  return (isNumeric(string) && int.parse(string) > 999)
-                      ? null
-                      : "Please enter a valid Code";
-                },
               ),
-            ),
+            ],
           ),
-          SizedBox(
-            height: screenSize.height / 30,
-          ),
-          InkWell(
-            ontap: () {
-              setState(() {
-                test = _pinPutController.text;
-              });
-              /*if (isNumeric(_pinPutController.text.trim())) {
-                widget.onSubmit(int.parse(_pinPutController.text.trim()));
-              }*/
-            },
-            child: Container(
-                width: screenSize.height * 0.6,
-                height: screenSize.height * 0.08,
-                child: Container(
-                  width: screenSize.width / 2,
-                  height: screenSize.height / 15,
-                  child: FittedBox(
-                      child: Text(
-                    "'" + test + "'",
-                    style:
-                        Theme.of(context).textTheme.headline1.merge(TextStyle(
-                              color: Colors.black87,
-                              fontWeight: FontWeight.w300,
-                            )),
-                  )),
+        ),
+        Container(
+          width: screenSize.height * 0.65,
+          child: RaisedButton(
+              color: Colors.white.withOpacity(0.20),
+              onPressed: () {
+                setState(() {
+                  // Move to "enter line code page"
+                  //if (isNumeric(_pinPutController.text)) {
+                  widget.onSubmit(int.parse(textbb));
+                  //}
+                });
+              },
+              child: Container(
+                width: screenSize.width / 2,
+                height: screenSize.height / 15,
+                child: FittedBox(
+                    child: Text(
+                  'Submit',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1
+                      .merge(TextStyle(color: Colors.black87)),
                 )),
-          ),
-        ],
-      ),
+              )),
+        ),
+        SizedBox(
+          height: screenSize.height / 30,
+        ),
+      ],
     );
   }
 }
